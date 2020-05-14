5c5b77614d4fbbb6da5baf688ff36db9c74cf8f6
Sviatoslav Prudkyi <sviatoslav.prudkyi@eu.nextiva.local>
2020-05-14 02:59:59 -0700

    created cards on the page with Classes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 window.addEventListener("DOMContentLoaded", () => {
 
-    // Создание ТАБОВ
+    // Tab Creation 
     function initTabs() {
         const tabs = document.querySelectorAll(".tabheader__item"),
             tabsContent = document.querySelectorAll(".tabcontent"),
@@ -59,7 +59,7 @@ window.addEventListener("DOMContentLoaded", () => {
                 minutes = Math.floor((t / (1000 * 60) % 60)),
                 seconds = Math.floor((t / 1000) % 60);
             return {
-                t,
+                total: t,
                 days,
                 hours,
                 minutes,
@@ -110,6 +110,7 @@ window.addEventListener("DOMContentLoaded", () => {
             modal.classList.remove('hide');
             // убирает прокрутку
             document.body.style.overflow = 'hidden';
+            clearInterval(modalTimerId); //отключает открытие модально по истечению времени 
 
             //option to work with toggle 
             // modal.classList.toggle('show')
@@ -137,6 +138,82 @@ window.addEventListener("DOMContentLoaded", () => {
                 closeModal();
             }
         });
+        //открывает модалку по истечению времени
+        // const modalTimerId = setTimeout(openModal, 5000);
+
+        // function showModalByScroll() {
+        //     if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+        //         openModal();
+        //         window.removeEventListener('scroll', showModalByScroll);
+        //     }
+        // }
+        // window.addEventListener('scroll', showModalByScroll)
     }
     initModal();
+
+    //class Usage for menu creation
+
+    function cardCreationClass() {
+
+        class MenuCard {
+            constructor(src, alt, title, descr, price, parentSelector) {
+                this.src = src;
+                this.alt = alt;
+                this.title = title;
+                this.descr = descr;
+                this.price = price;
+                this.parent = document.querySelector(parentSelector);
+                this.transfer = 27;
+                this.changeToUAH();
+            }
+            changeToUAH() {
+                this.price = this.price * this.transfer
+            }
+
+            render() {
+                let div = document.createElement('div');
+                div.innerHTML = ` 
+            <div class="menu__item">
+                <img src="${this.src}" alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div> 
+            </div>`;
+                this.parent.append(div);
+            }
+        }
+
+        new MenuCard(
+            "img/tabs/vegy.jpg",
+            "vegy",
+            'Меню "Фитнес"',
+            'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+            9,
+            '.menu .container'
+        ).render();
+
+        new MenuCard(
+            "img/tabs/elite.jpg",
+            "elite",
+            'Меню “Премиум”',
+            'В меню “Премиум” мы используем не только красивый дизайн упаковки, нои качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без походав ресторан!',
+            14,
+            '.menu .container'
+        ).render();
+
+        new MenuCard(
+            "img/tabs/post.jpg",
+            "post",
+            'Меню "Постное"',
+            'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствиепродуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+            9,
+            '.menu .container'
+        ).render();
+    }
+    cardCreationClass();
+    
 });
