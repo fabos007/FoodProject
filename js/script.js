da92e68ff4d6d223d34645fc4317c70df6ee5f3f
Sviatoslav Prudkyi <sviatoslav.prudkyi@eu.nextiva.local>
2020-05-14 23:35:34 -0700

    improved default settings for Classes

--- js/script.js
+++ js/script.js
@@ -156,12 +156,13 @@ window.addEventListener("DOMContentLoaded", () => {
     function cardCreationClass() {
 
         class MenuCard {
-            constructor(src, alt, title, descr, price, parentSelector) {
+            constructor(src, alt, title, descr, price, parentSelector, ...classes) {
                 this.src = src;
                 this.alt = alt;
                 this.title = title;
                 this.descr = descr;
                 this.price = price;
+                this.classes = classes;
                 this.parent = document.querySelector(parentSelector);
                 this.transfer = 27;
                 this.changeToUAH();
@@ -171,9 +172,16 @@ window.addEventListener("DOMContentLoaded", () => {
             }
 
             render() {
-                let div = document.createElement('div');
+                const div = document.createElement('div');
+                if (this.classes.length === 0) {
+                    this.div = 'menu__item';
+                    div.classList.add(this.div);
+                } else {
+                    this.classes.forEach(className => div.classList.add(className))
+                }
+
                 div.innerHTML = ` 
-            <div class="menu__item">
+            
                 <img src="${this.src}" alt=${this.alt}>
                 <h3 class="menu__item-subtitle">${this.title}</h3>
                 <div class="menu__item-descr">${this.descr}</div>
@@ -181,8 +189,7 @@ window.addEventListener("DOMContentLoaded", () => {
                 <div class="menu__item-price">
                     <div class="menu__item-cost">Цена:</div>
                     <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-                </div> 
-            </div>`;
+                </div> `;
                 this.parent.append(div);
             }
         }
@@ -193,7 +200,8 @@ window.addEventListener("DOMContentLoaded", () => {
             'Меню "Фитнес"',
             'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
             9,
-            '.menu .container'
+            '.menu .container',
+
         ).render();
 
         new MenuCard(
@@ -202,7 +210,8 @@ window.addEventListener("DOMContentLoaded", () => {
             'Меню “Премиум”',
             'В меню “Премиум” мы используем не только красивый дизайн упаковки, нои качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без походав ресторан!',
             14,
-            '.menu .container'
+            '.menu .container',
+
         ).render();
 
         new MenuCard(
@@ -211,9 +220,10 @@ window.addEventListener("DOMContentLoaded", () => {
             'Меню "Постное"',
             'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствиепродуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
             9,
-            '.menu .container'
+            '.menu .container',
+
         ).render();
     }
     cardCreationClass();
-    
+
 });
